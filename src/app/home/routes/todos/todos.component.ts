29afa4ed99909d4fe0dceb73f276ff49360bbe71
mastroda <mastrodascio.stefano@live.it>
2024-09-03 07:14:11 +0200

    aggiornato stile generale - card-todo - form-todo

--- src/app/home/routes/todos/todos.component.ts
+++ src/app/home/routes/todos/todos.component.ts
@@ -6,6 +6,8 @@ import { UtilsService } from 'src/app/utils.service';
 import { ModalTodoComponent } from '../../components/modal-todo/modal-todo.component';
 import { fadeAnimation } from 'src/app/animations';
 
+
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -14,15 +16,39 @@ import { fadeAnimation } from 'src/app/animations';
 })
 export class TodosComponent implements OnInit {
 
-  filtri: {} = {};
+  ricerca: string = '';
+  idStato: number = 0;
 
+  ordinamenti = ['creazione', 'titolo', 'descrizione'];
+
+  ordinamentoSelezionato: string = 'creazione';
 
   get stati$() {
     return this.dal.statiToDo$;
   }
 
   get todos$() {
-    return this.dal.todos$;
+    return this.dal.todos$.pipe(map(
+      todos => {
+        if (!todos) {
+          return [];
+        }
+        return todos
+          .filter(t => [t.titolo, t.descrizione].join('|').toLowerCase().includes(this.ricerca.toLowerCase()))
+          ?.sort((a, b) => {
+            switch (this.ordinamentoSelezionato) {
+              case 'creazione':
+                return a.dataCreazione > b.dataCreazione ? 1 : (a.dataCreazione === b.dataCreazione ? 0 : -1);
+              case 'descrizione':
+                return a.descrizione > b.descrizione ? 1 : (a.descrizione === b.descrizione ? 0 : -1);
+              case 'titolo':
+                return a.titolo > b.titolo ? 1 : (a.titolo === b.titolo ? 0 : -1);
+              default:
+                return 1;
+            }
+          });
+      }
+    ));
   }
 
   get isLoading$() {
@@ -31,15 +57,24 @@ export class TodosComponent implements OnInit {
 
   get todosByStato$() {
     return this.dal.statiToDo$.pipe(
+      map(stati => {
+        if (!stati) {
+          return [];
+        }
+        if (this.idStato > 0) {
+          return stati.filter(s => s.id === this.idStato);
+        }
+        return stati;
+      }),
       mergeMap(stati => {
-        return this.dal.todos$.pipe(map(
+        return this.todos$.pipe(map(
           todos => {
             if (!stati) {
               return [];
             }
             return stati.map(s => ({
               ...s,
-              todos: todos.filter(t => t.idStato === s.id)
+              todos: todos ? todos.filter(t => t.idStato === s.id) : []
             }))
           }
         ));
